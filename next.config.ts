9d68770dad3a7008c4a12b2587e64a2c36e8f2af
marhoaghware <marhoaghware@gmail.com>
2026-01-30 01:30:59 +0000

    fix: increase server action body size limit to 10mb

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
-      bodySizeLimit: '4mb',
+      bodySizeLimit: '10mb',
     },
   },
   images: {
